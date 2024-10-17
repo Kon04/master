42954e270f8150851bd42518361bb52a2f5d58e9
Kon <chankon0424@yahoo.co.jp>
2024-10-17 14:15:00 +0900

    a

--- b.py
+++ b.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 
 train_data_path = 'train_img.npz' #訓練データのパス
 test_data_path = 'test_img.npz'   #テストデータのパス
+batch_size = 32
+validation_split = 0.2
 
 #データセットの取得
 # npzファイルを読み込む(train)
@@ -19,20 +21,23 @@ y = train_data['label']
 # X:画像データ y:ラベルデータをセットでdataset化
 dataset_train = tf.data.Dataset.from_tensor_slices((X, y))
 
+# データセットのサイズを取得
+dataset_size = sum(1 for _ in dataset_train)
+
+# バリデーションセットのサイズを計算
+validation_size = int(validation_split * dataset_size)
+
 # シャッフル
 dataset_train = dataset_train.shuffle(buffer_size=len(X))
-print(X.shape, y.shape)
 
-# npzファイルを読み込む(test)
-test_data = np.load(test_data_path)
+# ミニバッチ化
+#（drop_remainder = Trueで端数切り捨て）
+dataset_train = dataset_train.batch(batch_size, drop_remainder=True)
 
-# 画像とラベルをそれぞれXとyに代入
-X = test_data['img']
-y = test_data['label']
+# 訓練セットとバリデーションセットに分割
+train_dataset = dataset_train.skip(validation_size)
+val_dataset = dataset_train.take(validation_size)
 
-# X:画像データ y:ラベルデータをセットでdataset化
-dataset_test = tf.data.Dataset.from_tensor_slices((X, y))
 
-# シャッフル
-dataset_test = dataset_test.shuffle(buffer_size=len(X))
-print(X.shape, y.shape)
+print(dataset_train.shape)
+# print(val_dataset.shape)
