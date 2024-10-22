f34c18ad279707607a89c3747832dd3144f76ec5
Kon <chankon0424@yahoo.co.jp>
2024-10-22 16:22:01 +0900

    h

--- model_test.py
+++ model_test.py
@@ -3,6 +3,8 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.models import load_model
@@ -24,16 +26,13 @@ test_data = np.load(test_data_path)
 X_test = test_data['img']
 y_test = test_data['label']
 
-#one-hotエンコーディング
-#y_test = to_categorical(y_test, num_classes=num_classes)
-
 #モデルと重みを復元
 model = load_model(model_path)
  
 #結果の表示
-# accuracy = model.evaluate(X_test, y_test, verbose=0)
-# print('test loss', accuracy[0])
-# print('test accuracy', accuracy[1])
+accuracy = model.evaluate(X_test, y_test, verbose=0)
+print('test loss', accuracy[0])
+print('test accuracy', accuracy[1])
 
 # テストデータでの予測
 y_pred = model.predict(X_test)
@@ -42,13 +41,24 @@ y_pred_classes = np.argmax(y_pred, axis=1) #配列の最大値のインデック
 # 混同行列の計算
 cm = confusion_matrix(y_test, y_pred_classes)
 
-# 各クラスの精度の計算
-class_accuracy = cm.diagonal() / cm.sum(axis=1)
+# ヒートマップの作成
+plt.figure(figsize=(8, 6))  # 図のサイズを指定
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', 
+            xticklabels=np.arange(num_classes), 
+            yticklabels=np.arange(num_classes))
 
-# pandas DataFrameに変換して表形式で表示
-accuracy_df = pd.DataFrame({
-    'クラス': np.arange(len(class_accuracy)),
-    '精度': class_accuracy
-})
+plt.title('Confusion Matrix')
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.show()
 
-print(accuracy_df)
+# # 各クラスの精度の計算
+# class_accuracy = cm.diagonal() / cm.sum(axis=1)
+
+# # pandas DataFrameに変換して表形式で表示
+# accuracy_df = pd.DataFrame({
+#     'クラス': np.arange(len(class_accuracy)),
+#     '精度': class_accuracy
+# })
+
+# print(accuracy_df)
