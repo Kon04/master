163f9ff215784d6ee306690c054e08743c77e1a8
Kon <chankon0424@yahoo.co.jp>
2024-11-01 14:23:32 +0900

    q

--- model_test.py
+++ model_test.py
@@ -38,8 +38,8 @@ print('test accuracy', accuracy[1])
 y_pred = model.predict(X_test)
 y_pred_classes = np.argmax(y_pred, axis=1) #配列の最大値のインデックスを取得
 
-# print("y_test:", y_test[:300])  # 最初の10個を表示
-# print("y_pred_classes:", y_pred_classes[:300])  # 最初の10個を表示
+print("y_test:", y_test[:1250])  # 最初の10個を表示
+print("y_pred_classes:", y_pred_classes[:1250])  # 最初の10個を表示
 
 # 混同行列の計算
 cm = confusion_matrix(y_test, y_pred_classes)
