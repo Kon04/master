d52837fce3379abe451ae9c5683425453f9efcdb
Kon <chankon0424@yahoo.co.jp>
2024-11-01 14:05:05 +0900

    d

--- model_test.py
+++ model_test.py
@@ -2,22 +2,18 @@
 
 import tensorflow as tf
 import numpy as np
-import pandas as pd
 import seaborn as sns
-import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
-from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.models import load_model
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
-from tensorflow.keras.optimizers import Adam, RMSprop
 from tensorflow.keras.utils import to_categorical
 
 #諸変数の定義
 num_classes = 5 #クラス数
 model_path = './model/incptionv3_weights.h5' #評価するモデルのパス
-test_data_path = 'test_img.npz'   #テストデータのパス
-num_test = 750 #テストデータの枚数
+test_data_path = 'test_unknown_img.npz'   #テストデータのパス
+save_name = 'incv3_confusion_matrix_unknown' #混合行列保存用のファイル名
+num_test = 1250 #テストデータの枚数
 #-------------------------------------------------------------
 
 # npzファイルを読み込む(test)
@@ -42,8 +38,8 @@ print('test accuracy', accuracy[1])
 y_pred = model.predict(X_test)
 y_pred_classes = np.argmax(y_pred, axis=1) #配列の最大値のインデックスを取得
 
-# print("y_test:", y_test[:750])  # 最初の10個を表示
-# print("y_pred_classes:", y_pred_classes[:750])  # 最初の10個を表示
+# print("y_test:", y_test[:300])  # 最初の10個を表示
+# print("y_pred_classes:", y_pred_classes[:300])  # 最初の10個を表示
 
 # 混同行列の計算
 cm = confusion_matrix(y_test, y_pred_classes)
@@ -58,7 +54,7 @@ sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
 plt.title('Confusion Matrix')
 plt.xlabel('Predicted Label')
 plt.ylabel('True Label')
-plt.savefig('incv3_confusion_matrix')
+plt.savefig(save_name)
 
 # # 各クラスの精度の計算
 # class_accuracy = cm.diagonal() / cm.sum(axis=1)
--- syori_dataset.py
+++ syori_dataset.py
@@ -8,13 +8,12 @@ import cv2
 import matplotlib.pyplot as plt
 
 #諸変数の定義
-img_size = 299
+img_size = 299 #画像サイズ
+save_name = 'test_unknown_img.npz' #生成したファイルの保存名
+train_dir = 'C:\\Kon\\master_reserch\\tensorflow\\test_dataset' #処理を加えるファイルのパス
+#test_dir = 'C:\\Kon\\master_reserch\\tensorflow\\train_dataset\\test'
 
-# ディレクトリの指定
-train_dir = 'C:\\Kon\\master_reserch\\tensorflow\\train_dataset\\train'
-test_dir = 'C:\\Kon\\master_reserch\\tensorflow\\train_dataset\\test'
-
-# カテゴリネームと番号を対応させた辞書を作成（ラベルは数値で保存します）
+# カテゴリネームと番号を対応させた辞書を作成（ラベルは数値）
 label_dict = {
     '50ml' : 0,
     '100ml' : 1,
@@ -59,49 +58,49 @@ for image_path in glob.glob(train_dir + '/*' + '/*'):
 
 #npzファイル作成
 # 第1引数はファイル名、第2引数以降は [キー名 = 配列]の形で書く
-np.savez('train_img.npz', img = Images_train, label = Labels_train)
+np.savez(save_name, img = Images_train, label = Labels_train)
 
-print('train終了')
+print('終了')
 
 #----------------------------------------------------
 #testについて処理
 # 画像とラベルを格納する空のリストを作る
-Images_test = []
-Labels_test = []
+# Images_test = []
+# Labels_test = []
 
-# 指定したディレクトリの画像パスを全て取得して処理していく
-for image_path in glob.glob(test_dir + '/*' + '/*'):
-    label_name = image_path.split('\\')[-2] 
+# # 指定したディレクトリの画像パスを全て取得して処理していく
+# for image_path in glob.glob(test_dir + '/*' + '/*'):
+#     label_name = image_path.split('\\')[-2] 
 
-    # ラベル番号の取得
-    label = label_dict[label_name]
-    print(label)
+#     # ラベル番号の取得
+#     label = label_dict[label_name]
+#     print(label)
   
-    # 画像の読み込み
-    img = cv2.imread(image_path)
+#     # 画像の読み込み
+#     img = cv2.imread(image_path)
 
-    # RGB形式に変換
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     # RGB形式に変換
+#     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # サイズ変換
-    img_resize = cv2.resize(img_rgb, (img_size,img_size))
+#     # サイズ変換
+#     img_resize = cv2.resize(img_rgb, (img_size,img_size))
 
-    # ndarray型に変換
-    numpy_img = np.array(img_resize)
+#     # ndarray型に変換
+#     numpy_img = np.array(img_resize)
 
-    # 指定した形にリシェイプ
-    numpy_img = numpy_img.reshape(img_size, img_size, 3)
+#     # 指定した形にリシェイプ
+#     numpy_img = numpy_img.reshape(img_size, img_size, 3)
 
-    # float32型にして255で割ることで[0,1]スケールの正規化
-    numpy_img = numpy_img.astype('float32')
-    numpy_img = numpy_img / 255
+#     # float32型にして255で割ることで[0,1]スケールの正規化
+#     numpy_img = numpy_img.astype('float32')
+#     numpy_img = numpy_img / 255
 
-    # 画像とラベルをリストに格納
-    Images_test.append(numpy_img)
-    Labels_test.append(label)
+#     # 画像とラベルをリストに格納
+#     Images_test.append(numpy_img)
+#     Labels_test.append(label)
 
-#npzファイル作成
-# 第1引数はファイル名、第2引数以降は [キー名 = 配列]の形で書く
-np.savez('test_img.npz', img = Images_test, label = Labels_test)
+# #npzファイル作成
+# # 第1引数はファイル名、第2引数以降は [キー名 = 配列]の形で書く
+# np.savez('test_img.npz', img = Images_test, label = Labels_test)
 
-print('test終了')
+# print('test終了')
