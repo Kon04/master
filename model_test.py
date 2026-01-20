22c061b50dc542a7d3d9e84a148143c02ec3514c
Kon <chankon0424@yahoo.co.jp>
2026-01-20 14:52:40 +0900

    v

--- model_test.py
+++ model_test.py
@@ -10,9 +10,9 @@ from tensorflow.keras.utils import to_categorical
 
 #諸変数の定義
 num_classes = 5 #クラス数
-model_path = './model/incptionv3_weights_real_simulation1800.h5' #評価するモデルのパス
+model_path = './model/incptionv3_weights_real_simulation900.h5' #評価するモデルのパス
 test_data_path = 'test_unknown_img.npz'   #テストデータのパス
-save_name = 'incv3_real_simulation1800_confusion_matrix_unknown3' #混合行列保存用のファイル名
+save_name = 'incv3_real_simulation900_confusion_matrix_unknown2' #混合行列保存用のファイル名
 num_test = 1250 #テストデータの枚数
 confusion_flag = 1 #混合行列を表示するかのフラグ（1の時表示）
 show_flag = 0 #分類を間違えた画像を表示するかのフラグ（1の時表示）
